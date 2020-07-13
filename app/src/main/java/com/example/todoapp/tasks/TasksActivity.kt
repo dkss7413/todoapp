4d323fd34fcb050cd514ea489508a719bf0d0ebc
sungJu <dkss7413@naver.com>
2020-07-13 23:02:02 +0900

    tasks_act.xml

--- app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
+++ app/src/main/java/com/example/todoapp/tasks/TasksActivity.kt
@@ -1,12 +1,13 @@
-package com.example.todoapp
+package com.example.todoapp.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.R
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.tasks_act)
     }
 }
